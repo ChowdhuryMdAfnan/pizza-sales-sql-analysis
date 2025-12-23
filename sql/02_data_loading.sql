c8d0ead547211a67c6cf955d1a9c1c61593eec06
Chowdhury Md Afnan <hridoychyy@gmail.com>
2025-12-23 16:47:01 +0600

    Create 02_data_loading.sql
    Add data loading instructions

---
+++ sql/02_data_loading.sql
@@ -0,0 +1,42 @@
+-- =========================================
+-- Pizza Sales Analysis
+-- Data Loading Instructions
+-- =========================================
+
+-- This project uses CSV files as the data source.
+-- The CSV files are located in the /data directory.
+
+-- Data can be loaded into MySQL using LOAD DATA INFILE
+-- or any MySQL client / GUI tool.
+
+-- Example: Load orders data
+-- LOAD DATA INFILE 'path/to/orders.csv'
+-- INTO TABLE orders
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+-- Example: Load order_details data
+-- LOAD DATA INFILE 'path/to/order_details.csv'
+-- INTO TABLE order_details
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+-- Example: Load pizzas data
+-- LOAD DATA INFILE 'path/to/pizzas.csv'
+-- INTO TABLE pizzas
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+-- Example: Load pizza_types data
+-- LOAD DATA INFILE 'path/to/pizza_types.csv'
+-- INTO TABLE pizza_types
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
